8a4036672d0ec20a060c66c3a47d9b4820ad9932
Samay Jain <63366920+samayjain21@users.noreply.github.com>
2021-09-21 15:06:42 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,20 +1,16 @@
 import pyspark
+from pyspark.sql.functions import greatest
 from pyspark.sql import SparkSession
+
 #Create SparkSession
 customSpark = SparkSession.builder.getOrCreate()
 
-# COMMAND ----------
 
-import pytest
-from pyspark.sql.functions import greatest
 
-# COMMAND ----------
-
-def func():
-  df = customSpark.createDataFrame([(2, 3)],["a","b"])
+def func(num1,num2):
+  df = customSpark.createDataFrame([(num1, num2)],["a","b"])
   df.show()
   df1 =  df.withColumn("max", greatest("a", "b")).select("max")
-#   print(df1.collect()[0][0])
   return df1.collect()[0][0]
 
 
