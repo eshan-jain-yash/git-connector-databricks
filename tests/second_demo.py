9ea0489643afe438dd7da5bf110465c5f748209d
Samay Jain <63366920+samayjain21@users.noreply.github.com>
2021-09-22 17:47:45 +0530

    Update and rename demo1.py to second_demo.py

--- tests/second_demo.py
+++ tests/second_demo.py
@@ -1,7 +1,7 @@
 import pyspark
 from pyspark.sql.functions import greatest
 
-def func(spark, num1,num2):
+def func(spark, num1, num2):
   df = spark.createDataFrame([(num1, num2)],["a","b"])  
   df1 =  df.withColumn("max", greatest("a", "b")).select("max")
   return df1.collect()[0][0]
