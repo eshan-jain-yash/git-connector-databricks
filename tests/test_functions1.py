349648e1a46ce4ccd2dc8f1f9bb7b766e78f312f
Samay Jain <63366920+samayjain21@users.noreply.github.com>
2021-09-22 17:50:54 +0530

    Update and rename test_functions1.py to test_second_demo.py

--- tests/test_functions1.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from demo1 import func
-import pyspark.sql.functions as F
-from test_conf import spark
-
-def test_func(spark):
-    num = func(spark, 2, 3)
-    assert num == 3
---
+++ tests/test_second_demo.py
@@ -0,0 +1,8 @@
+import pytest
+from second_demo import get_greatest_number
+import pyspark.sql.functions as F
+from pytest_pyspark_conf import spark
+
+def test_get_greatest_number(spark):
+    num = get_greatest_number(spark, 2, 3)
+    assert num == 3
