65657c68ef87922dbfe52f4a180a14496fb9fe61
Samay Jain <63366920+samayjain21@users.noreply.github.com>
2021-09-22 17:53:54 +0530

    Delete second_demo.py

--- tests/second_demo.py
+++
@@ -1,8 +0,0 @@
-import pyspark
-from pyspark.sql.functions import greatest
-
-def get_greatest_number(spark, num1, num2):
-  df = spark.createDataFrame([(num1, num2)],["a","b"])  
-  df1 =  df.withColumn("max", greatest("a", "b")).select("max")
-  return df1.collect()[0][0]
-
