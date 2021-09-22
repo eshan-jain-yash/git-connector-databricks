589a8bbe1dc5ce8368dedf50aaa7a0198bac9b7b
Samay Jain <63366920+samayjain21@users.noreply.github.com>
2021-09-22 17:53:47 +0530

    Delete first_demo.py

--- tests/first_demo.py
+++
@@ -1,7 +0,0 @@
-import pyspark
-import pyspark.sql.functions as F
-
-def remove_non_word_characters(spark, col):
-    df = spark.createDataFrame([(2, 3)],["a","b"])
-    return F.regexp_replace(col, "[^\\w\\s]+", "")
-
