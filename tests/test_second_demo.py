4fa698d64c1374b1bfed527d1510046d68390142
Eshan <eshanjain09@gmail.com>
2021-09-23 07:15:18 +0000

    third demo test and mark parametrize added

--- tests/test_second_demo.py
+++ tests/test_second_demo.py
@@ -3,6 +3,7 @@ from demo.second_demo import get_greatest_number
 import pyspark.sql.functions as F
 from pytest_pyspark_conf import spark
 
-def test_get_greatest_number(spark):
-    num = get_greatest_number(spark, 2, 3)
-    assert num == 3
+@pytest.mark.parametrize("num1,num2,expected", [(2,3,3),(5,3,5)])
+def test_get_greatest_number(spark,num1,num2,expected):
+    num = get_greatest_number(spark, num1, num2)
+    assert num == expected
