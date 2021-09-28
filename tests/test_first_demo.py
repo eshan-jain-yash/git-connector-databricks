828fff65eeb2af16bf1235653916f417694dff25
Samay Jain <eshanjain09@gmail.com>
2021-09-28 12:10:57 +0000

    imports correction

--- tests/test_first_demo.py
+++ tests/test_first_demo.py
@@ -1,5 +1,6 @@
 import pytest
-from demo.first_demo import remove_non_word_characters
+#from demo.first_demo import remove_non_word_characters
+import demo
 import pyspark.sql.functions as F
 from pytest_pyspark_conf import spark
 
@@ -11,7 +12,7 @@ def test_remove_non_word_characters(spark):
         (None, None)
     ]
     df = spark.createDataFrame(data, ["name", "expected_name"])\
-        .withColumn("clean_name", remove_non_word_characters(spark, F.col("name")))
+        .withColumn("clean_name", demo.remove_non_word_characters(spark, F.col("name")))
     a = df.select("clean_name").collect()
     b = df.select("expected_name").collect()
     assert a == b
